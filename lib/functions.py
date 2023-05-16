7e424673c61abdaaa22d15a408b1ff33cfb95d48
florence wangechi <florencewangechim@gmail.com>
2023-05-16 21:44:31 +0300

    completed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
-    pass
 
-def greet(name):
-    pass
+def greet_programmer():
+    print("Hello, programmer!")
+    return greet_programmer
+
+greet_programmer()
+
+def greet(name="Guido"):
+    print(f"Hello, {name}!")
+    return greet 
+
+greet()
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+    return greet_with_default
 
-def add(num1, num2):
-    pass
+greet_with_default()
 
-def halve(number):
-    pass
+def add(num1=45, num2=55):
+    print("added numters")
+    return num1 + num2
+
+add()    
+
+
+def halve(number=50):
+    return number / 2
+
+halve()
+    
---
+++ lib/solo.py
@@ -0,0 +1,6 @@
+def my_function(params):
+    print("running my_function")
+    return params + 1
+
+my_function_return_value=my_function(1)
+my_function_return_value()
